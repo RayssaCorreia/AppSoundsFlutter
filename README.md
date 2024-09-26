dbc2244d293d554045070d9d9d0743355aac7fdc
RayssaCorreia <rayssamicorreia@gmail.com>
2024-09-26 17:05:06 -0300

    [debug] start test 2

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # AppSoundsFlutter
 Tests of sound with Flutter 
 
-- test1 tutorial [youtube](https://www.youtube.com/watch?v=dspZNP5hc2w)
+## Docs
+
+- [Flutter](https://docs.flutter.dev/get-started/learn-flutter)
+
+- [Dart](https://dart.dev/guides)
+
+## Tests
+
+- [tutorial 1](https://www.youtube.com/watch?v=dspZNP5hc2w)
+    - [Record 4.4.4](https://pub.dev/packages/record/versions/4.4.4)
+    - [audioplayers 4.1.0](https://pub.dev/packages/audioplayers/versions/4.1.0/install)
+
+- [tutorial 2](https://www.youtube.com/watch?v=OHVKzKc5NgI)
+    - [widgetwisdom](https://widgetwisdom.com/)
--- sound/lib/main.dart
+++ sound/lib/main.dart
@@ -17,111 +17,32 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Sounds'),
+      home: const PlayAudio(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+
+class PlayAudio extends StatefulWidget {
+  const PlayAudio({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<PlayAudio> createState() => _PlayAudioState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  late Record audioRecord;
-  late AudioPlayer audioPlayer;
-  bool isRecording = false;
-  String audioPath = '';
+class _PlayAudioState extends State<PlayAudio> {
 
-  @override
-  void initState() {
-    audioRecord = Record();
-    audioPlayer = AudioPlayer();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    audioRecord.dispose();
-    audioPlayer.dispose();
-    super.dispose();
-  }
-
-  Future<void> startRecording() async{
-    try{
-      if(await audioRecord.hasPermission()){
-        await audioRecord.start();
-        setState(() {
-          isRecording = true;
-        });
-
-      }
-    }
-      catch(error){
-        print("Error Start Recording: $error");
-      }
-  }
-
-  Future<void> stopRecording() async{
-    try{
-        String? path = await audioRecord.stop();
-        setState(() {
-          isRecording = false;
-          audioPath = path!;
-        });
-    }
-    catch(error){
-      print("Error Stopping record: $error");
-    }
-  }
-
-  Future<void> playRecording() async{
-    try{
-      Source urlSource = UrlSource(audioPath);
-      await audioPlayer.play(urlSource);
-    }
-    
-      catch(error){
-        print('Error playing Recording: $error');
-      }
-  }
-
+  final player = AudioPlayer();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            if(isRecording)
-              Text(
-                'Recording in Progress',
-                style: TextStyle(
-                fontSize: 20,
-                ),
-              ),
-            ElevatedButton(
-                onPressed: isRecording ? stopRecording :startRecording,
-                child: isRecording ? const Text('Stop') : const Text('Start')
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            if(!isRecording && audioPath != null)
-            ElevatedButton(
-              onPressed: playRecording,
-              child: const Text("Play"),
-            ),
-          ],
-        ),
-      ),
+    return const ElevatedButton(
+      onPressed: () {
+        playSound();
+      },
+        child: const Text("Press me")
     );
+  }
+  void playSound() {
+  String audioPath = "audio/sound.mp3";
   }
 }
