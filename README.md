b6e37d83a01870a0bb6ab4741e64b1ca684d60ba
RayssaCorreia <rayssamicorreia@gmail.com>
2024-09-27 13:53:03 -0300

    [debug] test with just_audio

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
 # AppSoundsFlutter
 Tests of sound with Flutter 
 
-## Docs
+## Links 
 
+##### docs
 - [Flutter](https://docs.flutter.dev/get-started/learn-flutter)
-
 - [Dart](https://dart.dev/guides)
+- [Audio libraries](https://fluttergems.dev/audio/)
 
-## Tests
+##### curiosities
+- [What is beep file?](https://www.monolitonimbus.com.br/beep-music/)
+- [Beep codes](https://www.computerhope.com/beep.htm)
+- [Progam of modules beeps](https://linux.die.net/man/1/beep)
+- [Creat sounds](https://onlinesequencer.net/) 
 
+##### download free sound
+- [pixabay](https://pixabay.com/sound-effects/search/effect/)
+- [myinstants](https://www.myinstants.com/pt/search/?name=website)
+- [samplelib](https://samplelib.com/sample-mp3.html)
+- [elevenlabs](https://elevenlabs.io/)
+
+##### tests
 - [tutorial 1](https://www.youtube.com/watch?v=dspZNP5hc2w)
-    - [Record 4.4.4](https://pub.dev/packages/record/versions/4.4.4)
-    - [audioplayers 4.1.0](https://pub.dev/packages/audioplayers/versions/4.1.0/install)
-
 - [tutorial 2](https://www.youtube.com/watch?v=OHVKzKc5NgI)
-    - [widgetwisdom](https://widgetwisdom.com/)
-
 - [tutorial 3](https://medium.com/flutter-community/how-to-add-music-audio-to-your-flutter-app-dcb6162c32d7)
-
- ## Studings
-
-- What is beep file? [font](https://www.monolitonimbus.com.br/beep-music/)
-- Beep codes [font](https://www.computerhope.com/beep.htm)
-- Progam of modules beeps: [beep](https://linux.die.net/man/1/beep)
-- Creat sounds: [onlinesequencer](https://onlinesequencer.net/) 
--- sound/lib/main.dart
+++ sound/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:just_audio/just_audio.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,36 +14,128 @@ class MyApp extends StatelessWidget {
       title: 'sound',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+
       ),
-      home: const PlayAudio(),
+      home: const Home(),
     );
   }
 }
 
-class PlayAudio extends StatefulWidget {
-  const PlayAudio({super.key});
+class Home extends StatefulWidget{
+  const Home ({super.key});
 
   @override
-  State<PlayAudio> createState() => _PlayAudioState();
+  State<Home> createState() => _HomeState();
 }
 
-class _PlayAudioState extends State<PlayAudio> {
+class _HomeState extends State<Home> {
   final player = AudioPlayer();
+
+  String formatDuration(Duration d) {
+    final minutes = d.inMinutes.remainder(60);
+    final seconds = d.inSeconds.remainder(60);
+    return "${minutes.toString().padLeft(2, "0")}:${seconds.toString().padLeft(2,"0")}";
+  }
+
+  void handlePlayPause() {
+    if(player.playing){
+      player.pause();
+    } else {
+      player.play();
+    }
+  }
+
+  void handleSeek(double value) {
+    player.seek(Duration(seconds: value.toInt()));
+  }
+
+  Duration position = Duration.zero;
+  Duration duration = Duration.zero;
+
+  @override
+  void initState() {
+    super.initState();
+    player.setUrl('https://s3.amazonaws.com/scifri-episodes/scifri20181123-episode.mp3');
+    // player.setFilePath('sounds/Herege.mp3');
+
+    player.positionStream.listen((p) {
+      setState(() => position = p);
+    });
+
+    player.durationStream.listen((d) {
+      setState(() => duration = d!);
+    });
+
+    player.playerStateStream.listen((state) {
+      if (state.processingState == ProcessingState.completed) {
+        setState(() {
+          position = Duration.zero;
+        });
+        player.pause();
+        player.seek(position);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        playSound();
-      },
-        child: const Text("Press me")
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+              'Audio Player'
+          ),
+        ),
+      body: Padding(
+          padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(formatDuration(position)),
+            Slider(
+              min: 0.0,
+              max: duration.inSeconds.toDouble(),
+              value: position.inSeconds.toDouble(),
+              onChanged: handleSeek,
+            ),
+            Text(formatDuration(duration)),
+            IconButton(
+              icon: Icon(player.playing ? Icons.pause : Icons.play_arrow),
+              onPressed: handlePlayPause,
+            ),
+          ],
+        ),
+      ),
     );
   }
-
-  Future<void> playSound() async {
-    String audioPath = "sounds/sound2.mp3";
-    await player.play(AssetSource(audioPath));
-  }
-
 }
+
+
+
+//
+// class PlayAudio extends StatefulWidget {
+//   const PlayAudio({super.key});
+//
+//   @override
+//   State<PlayAudio> createState() => _PlayAudioState();
+// }
+//
+// class _PlayAudioState extends State<PlayAudio> {
+//   final player = AudioPlayer();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return ElevatedButton(
+//       onPressed: () {
+//         playSound();
+//       },
+//         child: const Text("Press me")
+//     );
+//   }
+//
+//   Future<void> playSound() async {
+//     String audioPath = "sounds/sound2.mp3";
+//     await player.play(AssetSource(audioPath));
+//   }
+//
+// }
